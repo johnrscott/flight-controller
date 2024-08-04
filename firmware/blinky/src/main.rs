a09db87e101e381058348cdbdd2b0402921a7607
John Scott <johnrscott0@gmail.com>
2024-08-04 16:23:01 +0100

    Got hal/blinky example working with cargo run.

--- firmware/blinky/src/main.rs
+++ firmware/blinky/src/main.rs
@@ -1,3 +1,5 @@
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_std]
 #![no_main]
 
@@ -7,18 +9,28 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+
+use stm32f7xx_hal as hal;
+use hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
 
-    
-    
+    // Have a look at the STM32F7xx HAL examples here:
+    // https://github.com/stm32-rs/stm32f7xx-hal/blob/main/examples/
+    let p = pac::Peripherals::take().unwrap();
+
+    let gpioi = p.GPIOI.split();
+    let mut led = gpioi.pi1.into_push_pull_output();
+
     loop {
-        // your code goes here
-	
+        for _ in 0..10_000 {
+            led.set_high();
+        }
+        for _ in 0..10_000 {
+            led.set_low();
+        }
     }
-
+    
 }
