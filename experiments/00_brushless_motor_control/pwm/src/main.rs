ad273c9a3dfc1a970ec855998008f324a184cf0b
John Scott <johnrscott0@gmail.com>
2024-08-09 22:55:13 +0100

    Refactored PWM into struct

--- experiments/00_brushless_motor_control/pwm/src/main.rs
+++ experiments/00_brushless_motor_control/pwm/src/main.rs
@@ -6,7 +6,83 @@
 use panic_halt as _;
 
 use cortex_m_rt::entry;
-use stm32f7xx_hal::{pac, prelude::*};
+use stm32f7xx_hal::{
+    pac::{self, Peripherals, TIM1, TIM12, TIM2, TIM3, TIM5},
+    prelude::*,
+    rcc::Clocks,
+    timer::{PwmChannel, C1, C2, C4},
+};
+
+struct ThreePhasePwm {
+    ch1: PwmChannel<TIM3, { C1 }>,  // PB4, pin 4 CN4
+    ch2: PwmChannel<TIM5, { C4 }>,  // PI0, pin 6 CN4
+    ch3: PwmChannel<TIM12, { C1 }>, // PH6, pin 7 CN4
+    ch4: PwmChannel<TIM2, { C1 }>,  // PA15, pin 2 CN7
+    ch5: PwmChannel<TIM1, { C1 }>,  // PA8, pin 3 CN7
+    ch6: PwmChannel<TIM12, { C2 }>, // PB15, pin 4 CN7
+}
+
+impl ThreePhasePwm {
+    fn new(dp: Peripherals, clocks: &Clocks) -> Self {
+        let gpioa = dp.GPIOA.split();
+        let gpiob = dp.GPIOB.split();
+        let gpioh = dp.GPIOH.split();
+        let gpioi = dp.GPIOI.split();
+
+        // CN4, pin 4
+        let channels = gpiob.pb4.into_alternate();
+        let mut ch0 = dp.TIM3.pwm_hz(channels, 20.kHz(), &clocks).split();
+        let max_duty = ch0.get_max_duty();
+        ch0.set_duty(max_duty / 2);
+
+        // CN4, pin 6
+        let channels = gpioi.pi0.into_alternate();
+        let mut ch1 = dp.TIM5.pwm_hz(channels, 20.kHz(), &clocks).split();
+        let max_duty = ch1.get_max_duty();
+        ch1.set_duty(max_duty / 3);
+
+        let channels = (gpioh.ph6.into_alternate(), gpiob.pb15.into_alternate());
+        let pwm = dp.TIM12.pwm_hz(channels, 20.kHz(), &clocks).split();
+        let (mut ch2, mut ch5) = pwm;
+
+        // CN4, pin 7
+        let max_duty = ch2.get_max_duty();
+        ch2.set_duty(max_duty / 4);
+
+        // CN7, pin 2
+        let channels = gpioa.pa15.into_alternate();
+        let mut ch3 = dp.TIM2.pwm_hz(channels, 20.kHz(), &clocks).split();
+        let max_duty = ch3.get_max_duty();
+        ch3.set_duty(max_duty / 5);
+
+        // CN7, pin 3
+        let channels = gpioa.pa8.into_alternate();
+        let mut ch4 = dp.TIM1.pwm_hz(channels, 20.kHz(), &clocks).split();
+        let max_duty = ch4.get_max_duty();
+        ch4.set_duty(max_duty / 6);
+
+        // CN7, pin 4
+        let max_duty = ch5.get_max_duty();
+        ch5.set_duty(max_duty / 7);
+
+        // Enable as a block to synchronise channels
+        // ch0.enable();
+        // ch1.enable();
+        // ch2.enable();
+        // ch3.enable();
+        // ch4.enable();
+        // ch5.enable();
+
+        Self {
+            ch1: ch0,
+            ch2: ch1,
+            ch3: ch2,
+            ch4: ch3,
+            ch5: ch4,
+            ch6: ch5,
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -15,59 +91,8 @@ fn main() -> ! {
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.freeze();
 
-	let gpioa = dp.GPIOA.split();
-	let gpiob = dp.GPIOB.split();
-        let gpioh = dp.GPIOH.split();
-        let gpioi = dp.GPIOI.split();
-	
         //let tim1_channels = (gpioa.pa8.into_alternate(),);
         //let tim2_channels = (gpioa.pa15.into_alternate(),);
-
-	// CN4, pin 4
-        let channels = gpiob.pb4.into_alternate();
-        let mut ch0 = dp.TIM3.pwm_hz(channels, 20.kHz(), &clocks).split();
-        let max_duty = ch0.get_max_duty();
-        ch0.set_duty(max_duty / 2);
-
-	// CN4, pin 6
-        let channels = gpioi.pi0.into_alternate();
-        let mut ch1 = dp.TIM5.pwm_hz(channels, 20.kHz(), &clocks).split();
-        let max_duty = ch1.get_max_duty();
-        ch1.set_duty(max_duty / 3);
-	
-        let channels = (gpioh.ph6.into_alternate(), gpiob.pb15.into_alternate());
-        let pwm = dp.TIM12.pwm_hz(channels, 20.kHz(), &clocks).split();
-        let (mut ch2 , mut ch5) = pwm;
-
-	// CN4, pin 7	
-	let max_duty = ch2.get_max_duty();
-        ch2.set_duty(max_duty / 4);
-
-	// CN7, pin 2
-        let channels = gpioa.pa15.into_alternate();
-        let mut ch3 = dp.TIM2.pwm_hz(channels, 20.kHz(), &clocks).split();
-        let max_duty = ch3.get_max_duty();
-        ch3.set_duty(max_duty / 5);
-
-	// CN7, pin 3
-        let channels = gpioa.pa8.into_alternate();
-        let mut ch4 = dp.TIM1.pwm_hz(channels, 20.kHz(), &clocks).split();
-        let max_duty = ch4.get_max_duty();
-        ch4.set_duty(max_duty / 6);
-	
-	// CN7, pin 4	
-        let max_duty = ch5.get_max_duty();
-        ch5.set_duty(max_duty / 7);
-
-	// Enable as a block to synchronise channels
-        ch0.enable();
-	ch1.enable();
-        ch2.enable();	
-        ch3.enable();
-        ch4.enable();
-        ch5.enable();
-
-	
     }
 
     loop {
