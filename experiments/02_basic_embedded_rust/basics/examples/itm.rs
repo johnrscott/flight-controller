a05fc7d1f6ec5189b6dcf97c8ea5de980676d902
John Scott <johnrscott0@gmail.com>
2024-08-20 22:05:15 +0100

    Working on clock frequency

--- experiments/02_basic_embedded_rust/basics/examples/itm.rs
+++ experiments/02_basic_embedded_rust/basics/examples/itm.rs
@@ -22,11 +22,26 @@ use panic_halt as _;
 use cortex_m::{iprintln, Peripherals};
 use cortex_m_rt::entry;
 
+use stm32f7xx_hal::sysctl;
+
 #[entry]
 fn main() -> ! {
     let mut p = Peripherals::take().unwrap();
+
+    // Wrap up the SYSCTL struct into an object with a higher-layer API
+    let mut sc = p.SYSCTL.constrain();
+    // Pick our oscillation settings
+    sc.clock_setup.oscillator = sysctl::Oscillator::Main(
+        sysctl::CrystalFrequency::_16mhz,
+        sysctl::SystemClock::UsePll(sysctl::PllOutputFrequency::_80_00mhz),
+    );
+    // Configure the PLL with those settings
+    let clocks = sc.clock_setup.freeze();
+
     let stim = &mut p.ITM.stim[0];
 
+    
+    
     iprintln!(stim, "Hello, world!");
 
     loop {}
--- experiments/02_basic_embedded_rust/basics/src/main.rs
+++ experiments/02_basic_embedded_rust/basics/src/main.rs
@@ -7,13 +7,28 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+use cortex_m::iprintln;
 use cortex_m_rt::entry;
+
+use stm32f7xx_hal as hal;
+use stm32f7xx_hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
 
+    if let (Some(dp), Some(mut cp)) = (
+        hal::pac::Peripherals::take(),
+        cortex_m::peripheral::Peripherals::take(),
+    ) {
+        // Set up the system clock.
+        let rcc = dp.RCC.constrain();
+	rcc.cfgr.sysclk(80_000_000.Hz()).freeze();
+
+	let stim = &mut cp.ITM.stim[0];
+
+	iprintln!(stim, "Hello World!");
+    }
+    
     loop {
         // your code goes here
     }
