a190e574b133e7d8dc5554944880eaa40779d033
John Scott <johnrscott0@gmail.com>
2024-08-24 22:43:34 +0100

    Added delays into the tasks

--- experiments/02_basic_embedded_rust/rtic-app/src/bin/minimal.rs
+++ experiments/02_basic_embedded_rust/rtic-app/src/bin/minimal.rs
@@ -4,17 +4,21 @@
 
 use test_app as _; // global logger + panicking-behavior + memory layout
 
-#[rtic::app(device = stm32f7xx_hal::pac, dispatchers = [TIM2,])]
+#[rtic::app(device = stm32f7xx_hal::pac, dispatchers = [TIM2, TIM3])]
 mod app {
 
-    use hal::rcc::{self, HSEClock};
-    use hal::timer::SysDelay;
+    const CLOCK_FREQ: u32 = 216_000_000;
+    
+    use hal::rcc::{self, HSEClock, Clocks};
     use stm32f7xx_hal as hal;
     use stm32f7xx_hal::prelude::*;
 
+    use rtic_monotonics::systick::prelude::*;
+
+    systick_monotonic!(Mono, 100);
+    
     #[shared]
     struct Shared {
-	delay: SysDelay
     }
 
     #[local]
@@ -24,8 +28,7 @@ mod app {
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("Starting RTIC init task");
 
-        // Cortex-M peripherals
-        let core = cx.core;
+	Mono::start(cx.core.SYST, CLOCK_FREQ);
 
         // Device specific peripherals
         let device = cx.device;
@@ -36,14 +39,13 @@ mod app {
         // and 216 MHz (the program will panic if out of range).
         let hse_cfg = HSEClock::new(25_000_000.Hz(), rcc::HSEClockMode::Bypass);
         let rcc = device.RCC.constrain();
-        let clocks = rcc.cfgr.hse(hse_cfg).sysclk(216_000_000.Hz()).freeze();
-
-	// Make a delay resource to share across tasks
-	let delay = core.SYST.delay(&clocks);
+        let _clocks = rcc.cfgr.hse(hse_cfg).sysclk(CLOCK_FREQ.Hz()).freeze();
 	
-        hello_loop::spawn().ok();
+        hello_slow_loop::spawn().ok();
+        hello_fast_loop::spawn().ok();
 
-        (Shared {delay}, Local {})
+	
+        (Shared {}, Local {})
     }
 
     // Optional idle, can be removed if not needed.
@@ -54,16 +56,21 @@ mod app {
         }
     }
     
-    // 
-    #[task(priority = 1, shared=[delay])]
-    async fn hello_loop(mut cx: hello_loop::Context) {
-
-	// 
+    #[task(priority = 1)]
+    async fn hello_slow_loop(_cx: hello_slow_loop::Context) {
 	loop {
-	    cx.shared.delay.lock(|delay| {
-		delay.delay_ms(1000u32);
-		defmt::info!("Hello!");
-	    });
+	    Mono::delay(1.secs()).await;
+	    defmt::info!("Hello every 1s!");
 	}
     }
+
+    #[task(priority = 1)]
+    async fn hello_fast_loop(_cx: hello_fast_loop::Context) {
+	loop {
+	    Mono::delay(5.secs()).await;
+	    defmt::info!("Hello every 5s!");
+	}
+    }
+
+    
 }
