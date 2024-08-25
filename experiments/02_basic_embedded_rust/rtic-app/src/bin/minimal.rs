eab9f1942249fab3dc239709aa9d78cba8a9189e
John Scott <johnrscott0@gmail.com>
2024-08-25 21:32:47 +0100

    Got noline uart serial working

--- experiments/02_basic_embedded_rust/rtic-app/src/bin/minimal.rs
+++ experiments/02_basic_embedded_rust/rtic-app/src/bin/minimal.rs
@@ -11,10 +11,14 @@ mod app {
     
     use hal::pac::USART1;
     use hal::rcc::{self, HSEClock};
-    use hal::serial::{self, Serial};
+    use hal::serial::{self, Serial, Rx, Tx};
+    use noline::builder::EditorBuilder;
+    use noline::error::NolineError;
+    use noline::sync_io::IO;
     use stm32f7xx_hal as hal;
     use stm32f7xx_hal::prelude::*;
-
+    use embedded_io::{Read, Write, ErrorType};
+    
     use rtic_monotonics::systick::prelude::*;
 
     systick_monotonic!(Mono, 100);
@@ -25,10 +29,73 @@ mod app {
 
     #[local]
     struct Local {
-	tx: serial::Tx<USART1>,
-	rx: serial::Rx<USART1>,
+	io: IO<SerialWrapper>
     }
 
+    struct SerialWrapper {
+	rx: Rx<USART1>,	
+	tx: Tx<USART1>,
+    }
+
+    impl SerialWrapper {
+
+	fn new(rx: serial::Rx<USART1>, tx: serial::Tx<USART1>) -> Self {
+	    Self { rx, tx }
+	}
+    }
+
+    impl ErrorType for SerialWrapper {
+	type Error = NolineError;
+    }
+    
+    impl Read for SerialWrapper {
+	fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+	    // Really basic implementation, just one char at a time
+	    if buf.len() == 0 {
+		Ok(0)
+	    } else {
+
+		// This function blocks, so just wait for char
+		loop {
+		    match self.rx.read() {
+			Ok(ch) => {			    
+			    buf[0] = ch;
+			    defmt::info!("Read: {}", ch);
+			    // Once a char is received, just return it
+			    return Ok(1);
+			},
+			Err(_) => {},
+		    }
+		}
+	    }
+	}
+    }
+
+    impl Write for SerialWrapper {
+	fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+	    if buf.len() == 0 {
+		Ok(0)
+	    } else {
+		let mut sent_counter: usize = 0;
+		for ch in buf {
+		    // Loop calling write until it succeeds. The HAL
+		    // serial write call does not block if a character
+		    // is currently being transmitted; it returns without
+		    // sending anything. Keep retrying until ch is sent.
+		    //while let Err(_) = self.tx.write(*ch) {};
+		    
+		    defmt::info!("write: {}", *ch);
+		    sent_counter += 1;
+		}
+		Ok(sent_counter)
+	    }
+	}
+
+	fn flush(&mut self) -> Result<(), Self::Error> {
+	    Ok(())
+	}
+    }
+    
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("Starting RTIC init task");
@@ -52,7 +119,7 @@ mod app {
 	let tx = gpioa.pa9.into_alternate();
 	let rx = gpiob.pb7.into_alternate();
 
-	let mut serial = Serial::new(
+	let serial = Serial::new(
             device.USART1,
             (tx, rx),
             &clocks,
@@ -62,14 +129,25 @@ mod app {
             },
 	);
 
-	// Listen for a received character
-	serial.listen(serial::Event::Rxne);
-
 	let (tx, rx) = serial.split();
+
+	let mut wrapper = SerialWrapper::new(rx, tx);
+	// let mut buf: [u8; 16] = ['t' as u8; 16];
+	// wrapper.write(&mut buf).ok();
+	//let mut buf: [u8; 16] = [0; 16];  
+	// loop {
+	//     wrapper.read(&mut buf);
+	//     defmt::info!("{}", buf);
+	// }
 	
-        hello_loop::spawn().ok();
+	let io = IO::new(wrapper);
 	
-        (Shared {}, Local { tx, rx })
+        //hello_loop::spawn().ok();
+	serial_task::spawn().ok();
+
+	defmt::info!("Ending init task");
+	
+        (Shared {}, Local {io})
     }
 
     // Optional idle, can be removed if not needed.
@@ -88,9 +166,18 @@ mod app {
 	}
     }
     
-    #[task(binds = USART1, priority = 1, local=[tx, rx])]
-    fn serial_task(cx: serial_task::Context) {
-        let received = cx.local.rx.read().unwrap_or('E' as u8);
-        cx.local.tx.write(received).ok();
+    #[task(priority = 1, local=[io])]
+    async fn serial_task(cx: serial_task::Context) {
+
+	defmt::info!("Starting serial task");
+	let mut io = cx.local.io;
+	let mut editor = EditorBuilder::new_static::<128>()
+            .with_static_history::<128>()
+            .build_sync(&mut io)
+            .unwrap();
+	
+	while let Ok(line) = editor.readline("> ", &mut io) {
+            defmt::info!("Read: '{}'", line);
+	}
     }    
 }
